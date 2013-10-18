a88975d67f06ff1bcf623eda6acc05cdf49e4ad5
chfw <wangc_2011@hotmail.com>
2013-10-18 10:18:05 +0100

    updated according to M.H.'s comment

--- api.py
+++ api.py
@@ -1,7 +1,12 @@
 import flask
+from data import test
+from models import db
 
 api = flask.Blueprint("api",__name__,template_folder="templates")
 
 @api.route("/")
 def helloworld():
-    return "hello world"
+    t = test(ab="aa")
+    db.session.add(t)
+    db.session.commit()
+    return t.ab+str(t.id)
--- application.py
+++ application.py
@@ -7,11 +7,10 @@ def create_app():
     app = flask.Flask("app")
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
     app.register_blueprint(api)
-    db.app=app
     db.init_app(app)
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     return app
-
 
 if __name__ == '__main__':
     app = create_app()
